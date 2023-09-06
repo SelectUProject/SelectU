5889b59ec9ad69d5741c6f474a80dcaca0aba68e
Will Anderson <88468039+WillAndoo@users.noreply.github.com>
2023-09-06 19:43:36 +1000

    Some CSS

--- Client/src/app/components/shared/short-view-my-applications/short-view-my-applications.component.ts
+++ Client/src/app/components/shared/short-view-my-applications/short-view-my-applications.component.ts
@@ -10,6 +10,7 @@ export class ShortViewMyApplicationsComponent{
   @Input() application: MyApplicationShortViewDTO;
 
   @Input() statusText: string;
+  @Input() statusIcon: string; 
 
   
 
@@ -17,15 +18,30 @@ export class ShortViewMyApplicationsComponent{
     // Define your logic here to determine the CSS class based on statusText
     const classes: string[] = [];
     if (this.application.status === 'accepted') {
-      classes.push('accepted');
+      classes.push("border rounded p-3 text-center fa-solid fa-check-square");
     } else if (this.application.status === 'denied') {
       classes.push('denied');
     } else if (this.application.status === 'uploaded') {
-      classes.push('uploaded');
+      classes.push("border rounded p-3 text-center fa-solid fa-cloud-upload");
     } else {
-      classes.push('pending');
+      classes.push('border rounded p-3 text-center fa-solid fa-spinner');
     }
     return classes;
+  }
+
+  getStatusIcon(): string[] {
+    const classes: string[] = [];
+    if (this.application.status === 'accepted') {
+      classes.push("border rounded p-3 text-center fa-solid fa-check-square");
+    } else if (this.application.status === 'denied') {
+      classes.push('denied');
+    } else if (this.application.status === 'uploaded') {
+      classes.push("border rounded p-3 text-center fa-solid fa-cloud-upload");
+    } else {
+      classes.push('border rounded p-3 text-center fa-solid fa-spinner');
+    }
+
+    return classes; 
   }
 
 }
