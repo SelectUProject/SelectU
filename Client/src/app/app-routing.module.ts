a412dd6d09243097145faf61c7675f955916e862
Daniel Ryan <36195344+Tornadic@users.noreply.github.com>
2023-10-11 21:49:36 +1100

    Update app-routing.module.ts

--- Client/src/app/app-routing.module.ts
+++ Client/src/app/app-routing.module.ts
@@ -58,7 +58,7 @@ const routes: Routes = [
         path: 'update-account',
         canActivate: [AuthGuard],
         component: UpdateUserProfilePageComponent,
-      }
+      },
       {
         path: 'invite-user',
         component: UserInvitePageComponent,
