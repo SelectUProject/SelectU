5aa56d50229c676af7dd3d72515054685e921d8e
Daniel Ryan <36195344+Tornadic@users.noreply.github.com>
2023-10-15 14:12:36 +1100

    oops

--- Client/src/environments/environment.ts
+++ Client/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   admissionName: 'Scholarships',
-  apiBaseUri: 'https://selectu-frontend.azurewebsites.net',
+  apiBaseUri: 'https://selectu-backend.azurewebsites.net',
   googleClientId:
     '916817943783-uep7ecjsr44mroo06ig665nmsm0aad9t.apps.googleusercontent.com',
   googleClientSecret: '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',
