60331bc06ccc43df4942b18a8ca3d711924244f4
Darcy Giblin <darcygiblin@gmail.com>
2023-10-17 18:18:19 +1100

    Fixed update scholarship

--- Client/src/app/components/pages/edit-scholarship-page/edit-scholarship-page.component.ts
+++ Client/src/app/components/pages/edit-scholarship-page/edit-scholarship-page.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 
 import { ScholarshipUpdateDTO } from 'src/app/models/ScholarshipUpdateDTO';
@@ -10,11 +15,12 @@ import { ScholarshipFormSectionListService } from 'src/app/services/scholarship-
 @Component({
   selector: 'app-edit-scholarship-page',
   templateUrl: './edit-scholarship-page.component.html',
-  styleUrls: ['./edit-scholarship-page.component.scss']
+  styleUrls: ['./edit-scholarship-page.component.scss'],
 })
 export class EditScholarshipPageComponent implements OnInit {
   scholarshipId = this._route.snapshot.paramMap.get('id');
   editScholarshipForm: FormGroup;
+  orignalImage: string;
   scholarshipImg: FormData = new FormData();
 
   constructor(
@@ -32,70 +38,74 @@ export class EditScholarshipPageComponent implements OnInit {
   }
 
   initializeForm(): void {
-    this.editScholarshipForm = this._formBuilder.group(
-      {
-        id: new FormControl('', Validators.required),
-        scholarshipCreatorId: new FormControl('', Validators.required),
-        school: new FormControl('', Validators.required),
-        imageURL: new FormControl('', Validators.required),
-        value: new FormControl('', Validators.required),
-        shortDescription: new FormControl('', Validators.required),
-        description: new FormControl('', Validators.required),
-        scholarshipFormTemplate: [],
-        city: new FormControl('', Validators.required),
-        state: new FormControl('', Validators.required),
-        startDate: new FormControl('', Validators.required),
-        endDate: new FormControl('', Validators.required)
-      }
-    );
+    this.editScholarshipForm = this._formBuilder.group({
+      id: new FormControl('', Validators.required),
+      scholarshipCreatorId: new FormControl('', Validators.required),
+      school: new FormControl('', Validators.required),
+      imageURL: new FormControl('', Validators.required),
+      value: new FormControl('', Validators.required),
+      shortDescription: new FormControl('', Validators.required),
+      description: new FormControl('', Validators.required),
+      scholarshipFormTemplate: [],
+      city: new FormControl('', Validators.required),
+      state: new FormControl('', Validators.required),
+      startDate: new FormControl('', Validators.required),
+      endDate: new FormControl('', Validators.required),
+    });
   }
 
   async populateForm(): Promise<void> {
     await this._scholarshipService
       .getScholarshipDetails(this.scholarshipId ? this.scholarshipId : '')
-        .then((response) => {
-          // Individually adding the form section to the array
-          response.scholarshipFormTemplate.forEach(formSection => {
-            this._scholarshipFormSectionListService.add(formSection);
-          });
-
-          // Updating form values from the response
-          this.editScholarshipForm.patchValue(
-            {
-              id: response.id,
-              scholarshipCreatorId: response.scholarshipCreatorId,
-              school: response.school,
-              imageURL: response.imageURL,
-              value: response.value,
-              shortDescription: response.shortDescription,
-              scholarshipFormTemplate: this._scholarshipFormSectionListService.formSections,
-              description: response.description,
-              city: response.city,
-              state: response.state,
-              startDate: new Date(response.startDate),
-              endDate: new Date(response.endDate)
-            }
-          );
-        })
-        .catch((response) => {
-          this._toastService.show(response.message, { classname: 'bg-danger text-light'} );
-          this._router.navigate(['/manage-scholarships']);
-        })
+      .then((response) => {
+        // Individually adding the form section to the array
+        response.scholarshipFormTemplate.forEach((formSection) => {
+          this._scholarshipFormSectionListService.add(formSection);
+        });
+        this.orignalImage = response.imageURL;
+        // Updating form values from the response
+        this.editScholarshipForm.patchValue({
+          id: response.id,
+          scholarshipCreatorId: response.scholarshipCreatorId,
+          school: response.school,
+          imageURL: '',
+          value: response.value,
+          shortDescription: response.shortDescription,
+          scholarshipFormTemplate:
+            this._scholarshipFormSectionListService.formSections,
+          description: response.description,
+          city: response.city,
+          state: response.state,
+          startDate: new Date(response.startDate),
+          endDate: new Date(response.endDate),
+        });
+      })
+      .catch((response) => {
+        this._toastService.show(response.message, {
+          classname: 'bg-danger text-light',
+        });
+        this._router.navigate(['/manage-scholarships']);
+      });
   }
 
   updateScholarship() {
-    let editScholarshipForm = <ScholarshipUpdateDTO>this.editScholarshipForm.value;
+    let editScholarshipForm = <ScholarshipUpdateDTO>(
+      this.editScholarshipForm.value
+    );
 
+    editScholarshipForm.imageURL = this.orignalImage;
     this._scholarshipService
       .updateScholarship(editScholarshipForm)
       .then((response) => {
         // uploading an image after the scholarship has been created
-        this._scholarshipService
-          .uploadScholarshipImg(this.scholarshipId, this.scholarshipImg);
+        this._scholarshipService.uploadScholarshipImg(
+          this.scholarshipId,
+          this.scholarshipImg
+        );
 
         this._toastService.show(response.message, {
           classname: 'bg-success text-light',
-          delay: 5000
+          delay: 5000,
         });
 
         this._router.navigate(['/manage-scholarships']);
@@ -103,13 +113,17 @@ export class EditScholarshipPageComponent implements OnInit {
       .catch((response) => {
         this._toastService.show(response.error.message ?? response.error, {
           classname: 'bg-danger text-light',
-          delay: 5000
+          delay: 5000,
         });
       });
   }
 
   deleteScholarship() {
-    const deleteConfirm = confirm(`Are you sure you want to delete this scholarship:\n"${this.editScholarshipForm.get("shortDescription")?.value}"?`);
+    const deleteConfirm = confirm(
+      `Are you sure you want to delete this scholarship:\n"${
+        this.editScholarshipForm.get('shortDescription')?.value
+      }"?`
+    );
 
     if (deleteConfirm) {
       this._scholarshipService
